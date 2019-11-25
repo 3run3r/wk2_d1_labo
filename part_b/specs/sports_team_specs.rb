75ad8298166631785c8cd49e9ccd6be2e1af83c0
Matteo Fusillo <matt.fusillo@gmail.com>
2019-11-25 16:55:44 +0000

    finished part b, still to review

--- part_b/specs/sports_team_specs.rb
+++ part_b/specs/sports_team_specs.rb
@@ -6,24 +6,52 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestTeam < MiniTest::Test
   def test_can_get_team_name
-    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti")
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
     assert_equal("Milan", team.team_name)
   end
 
   def test_can_get_players
-    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti")
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
     assert_equal(["Maldini", "Kaka"], team.players)
   end
 
   def test_can_get_coach
-    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti")
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
     assert_equal("Ancelotti", team.coach)
   end
 
+  def test_can_get_points
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
+    assert_equal(0, team.points)
+  end
+
   def test_can_set_new_coach
-    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti")
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
     team.coach = "Mourinho"
     assert_equal("Mourinho", team.coach)
+  end
+
+  def test_can_add_new_player_to_players
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
+    team.add_new_player("Ronaldo")
+    assert_equal(["Maldini", "Kaka", "Ronaldo"], team.players)
+  end
+
+  def test_is_player_in_team
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
+    assert_equal(true, team.check_player_in_team("Kaka"))
+  end
+
+  def test_if_team_has_won
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
+    team.change_points_if_win_or_lost("Win")
+    assert_equal(1, team.points)
+  end
+
+  def test_if_team_has_lost
+    team = Team.new("Milan", ["Maldini", "Kaka"], "Ancelotti", 0)
+    team.change_points_if_win_or_lost("Loss")
+    assert_equal(0, team.points)
   end
 
 end
--- part_b/sports_team.rb
+++ part_b/sports_team.rb
@@ -1,10 +1,30 @@
 class Team
   attr_accessor :coach
-  attr_reader :team_name, :players
+  attr_reader :team_name, :players, :points
 
-  def initialize(team_name, players, coach)
+  def initialize(team_name, players, coach, points)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = points
   end
+
+  def add_new_player(player)
+    @players << player
+  end
+
+  def check_player_in_team(player_to_check)
+    for player in @players
+      if player == player_to_check
+        return true
+      end
+    end
+  end
+
+  def change_points_if_win_or_lost(result)
+    @points += 1 if result == "Win"
+    @points += 0 if result == "Loss"
+  end
+
+
 end
