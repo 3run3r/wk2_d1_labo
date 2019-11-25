1e5ef2a165c2b502943c75ba214636e6abbec746
Matteo Fusillo <matt.fusillo@gmail.com>
2019-11-25 19:02:30 +0000

    refactored part_b

--- part_b/sports_team.rb
+++ part_b/sports_team.rb
@@ -1,12 +1,11 @@
 class Team
-  attr_accessor :coach
-  attr_reader :team_name, :players, :points
+  attr_accessor :coach, :team_name, :players, :points
 
-  def initialize(team_name, players, coach, points)
+  def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
-    @points = points
+    @points = 0
   end
 
   def add_new_player(player)
@@ -19,11 +18,12 @@ class Team
         return true
       end
     end
+    return false
   end
 
   def change_points_if_win_or_lost(result)
     @points += 1 if result == "Win"
-    @points += 0 if result == "Loss"
+    @points if result == "Loss"
   end
 
 
